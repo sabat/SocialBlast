4c703f7c9876c02d74b2463f9117069b248da3bd
Steve Abatangle <sabat@area51.org>
2012-08-27 15:23:07 -0700

    include new TimedCounter class

---
+++ lib/social_blast/timed_counter.rb
@@ -0,0 +1,73 @@
+class TimedCounter
+  include ShmStore
+
+  DEFAULT_INTERVAL = 60 # minutes
+
+  def reset
+    set_val('timestamp', Time.now)
+    set_val('counter', 0) 
+  end
+
+  def value=(v)
+    set_val('counter', v)
+  end
+
+  def value
+    val = get_val('counter')
+    val.nil? ? reset : val
+  end
+
+  def timestamp=(v)
+    set_val('timestamp', v)
+  end
+
+  def timestamp
+    val = get_val('timestamp')
+    if val.nil?
+      reset
+      get_val('timestamp')
+    else
+      val
+    end
+  end
+
+  def interval=(v)
+    @interval = v
+  end
+
+  def interval
+    @interval || DEFAULT_INTERVAL
+  end
+
+  def increment
+    if interval_expired?
+      reset
+    else
+      set_val('counter', self.value + 1)
+    end
+    get_val 'counter'
+  end
+
+  def to_s
+    self.value.to_s
+  end
+
+  def to_i
+    self.value.to_i
+  end
+
+  #
+
+  private
+
+  def minute_difference
+    diff = (Time.now - self.timestamp) / 60
+    diff.to_i
+  end
+
+  def interval_expired?
+    minute_difference > self.interval
+  end
+
+end
+
